527980c599b68e2349390cdd1dc660d75545f629
Mateusz <Mateusz@niedziala.pl>
2018-02-05 13:38:22 +0100

    read data (read.csv)

--- src/main.R
+++ src/main.R
@@ -1,2 +1,28 @@
-hello <- "hello"
-print (hello)
+library(readr)
+colClass <-
+  c("integer",
+    "character",
+    "character",
+    "character",
+    "factor",
+    "factor",
+    "factor")
+colNames <-
+  c("learner_id",
+    "country",
+    "in_course",
+    "unit",
+    "avg_score",
+    "completion",
+    "inv_rate")
+data2018 <-
+  read.csv(
+    file = "./data/data2018.csv",
+    header = TRUE,
+    sep = ";",
+    dec = ".",
+    na.strings=c(""),
+    colClasses = colClass,
+    col.names = colNames,
+    encoding = "UTF-8-BOM"
+  )
