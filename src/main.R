be1e1dff007de722301246b08d15c9b124fddc89
Mateusz <Mateusz@niedziala.pl>
2018-02-05 16:53:26 +0100

    remove summary file and function for that

--- src/main.R
+++ src/main.R
@@ -1,41 +1,5 @@
 library(readr)
-library(ggplot2) 
-
-#' Initialize summary file - reset content of file
-#'
-#' @param x path to summary file
-#' @examples
-#' summaryINIT(pathToFile)
-summaryINIT <- function(x) {
-  sink(x)
-}
-
-#' Enable capture output (like print) to summary file - append future content to this file
-#'
-#' @param x path to summary file
-#' @examples
-#' summaryON(pathToFile)
-summaryON <- function(x) {
-  sink(x, append = TRUE)
-}
-
-#' Disable capture output (like print) from any file
-#' Enalbe capture output (like print) to console
-#'
-#' @examples
-#' summaryOFF(pathToFile)
-summaryOFF <- function() {
-  sink()
-}
-
-### MAIN
-
-#path to summary file
-summaryFile = "./summary.txt"
-
-#init summary file
-summaryINIT(summaryFile)
-summaryOFF()
+library(ggplot2)
 
 # read data
 colClass <-
@@ -71,28 +35,39 @@ data2018 <-
 
 ## empty column
 isNaCol <- data.frame(sapply(data2018,
-                  function(x)
-                    print(sum(is.na(x)))))
-colnames(isNaCol) <- c("Empty values")
-summaryON(summaryFile)
-print("Table with empty values in every column:")
-print(isNaCol)
-summaryOFF()
+                             function(x)
+                               print(sum(is.na(
+                                 x
+                               )))))
+colnames(isNaCol) <- c("Empty values") # add columns names
 
 ## remove rows with NA in learner_id
-data2018 <- data2018[!(is.na(data2018$learner_id)),]
+data2018 <- data2018[!(is.na(data2018$learner_id)), ]
 
 ## plot country
-number_of_country <- length(unique(data2018$country))
-country_vec <- table(data2018$country)
-country_fisrt_10 <- data.frame(head(sort(country_vec,decreasing = TRUE),n=10))
-country_last_1 <- data.frame(country_vec[country_vec == 1])
-colNamesCountry <- c("Country","Frequency")
-colnames(country_fisrt_10) <- colNamesCountry
+number_of_country <-
+  length(unique(data2018$country)) # count unique country name
+
+country_vec <-
+  table(data2018$country) # create table from country only
+
+country_fisrt_10 <-
+  data.frame(head(sort(country_vec, decreasing = TRUE), n = 10)) # get first 10 country with the most numer of participants
+country_last_1 <-
+  data.frame(country_vec[country_vec == 1]) # get country with only 1 participant
+
+colNamesCountry <- c("Country", "Frequency") # create columns names
+colnames(country_fisrt_10) <- colNamesCountry # add columns name
 colnames(country_last_1) <- colNamesCountry
 
-plot_country_10 <- ggplot(data=country_fisrt_10, aes(x=Country, y=Frequency)) +
-  geom_bar(stat="identity",width=0.8, fill="cadetblue3") +
-  geom_text(aes(label=Frequency), vjust=-0.2, color="black", size=3.5)+
+# create plot (bar) from first 10 country
+plot_country_10 <-
+  ggplot(data = country_fisrt_10, aes(x = Country, y = Frequency)) +
+  geom_bar(stat = "identity", width = 0.8, fill = "cadetblue3") +
+  geom_text(
+    aes(label = Frequency),
+    vjust = -0.2,
+    color = "black",
+    size = 3.5
+  ) +
   theme_minimal()
-
