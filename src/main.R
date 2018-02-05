dd34652331d2d8b49ee1e475ad239aedcf7f3920
Mateusz <Mateusz@niedziala.pl>
2018-02-05 20:40:39 +0100

    add function to plot graph (works for me;p)

--- src/main.R
+++ src/main.R
@@ -1,15 +1,72 @@
 library(readr)
 library(ggplot2)
 
+#functions
+
+#' Plot bar graph from data frame
+#' 
+#' @param x A data.frame.
+#' @param ax A name of X axis.
+#' @param ay A name of Y axis.
+#' @param angle A angle of X axis ticks (in degrees)
+#' @return The plot of \code{x} with \code{ax} horizontal axis and \code{ay} vertical axis.
+plot_bar <- function(x,ax,ay,angle=0){
+  plot_temp <-
+    ggplot(data = x, aes_string(x = ax, y = ay)) +
+    geom_bar(stat = "identity", width = 0.8, fill = "cadetblue3") +
+    geom_text(
+      aes_string(label = ay),
+      vjust = -0.2,
+      color = "black",
+      size = 3.5
+    ) +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle=angle))
+  return(plot_temp)
+}
+
+#' Plot pie graph from data frame
+#' 
+#' @param x A data.frame.
+#' @param ax (string with quotes) A name of X axis.  (what will be in legend)
+#' @param ay (string with quotes) A name of Y axis. (number of every item in legend)
+#' @return The pie plot of \code{x}.
+plot_pie <- function(x,ax,ay){
+  # create blank theme (for pie chart)
+  blank_theme <- theme_minimal()+
+    theme(
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      panel.border = element_blank(),
+      panel.grid=element_blank(),
+      axis.ticks = element_blank(),
+      plot.title=element_text(size=14, face="bold")
+    )
+  # prepare variable to plot
+  number_all_row <- sum(x[[ay]])
+  yy <- x[[ay]]/2 + c(0, cumsum(x[[ay]])[-length(x[[ay]])])
+  yy_percent <- paste(signif(x[[ay]]/number_all_row, 3) * 100, "%", sep="") 
+  # pie graph
+  plot_temp <-
+    ggplot(data = x, aes_string(x = "\"\"",y = ay, fill = ax)) +
+    geom_bar(width = 1, stat = "identity") +
+    coord_polar("y", start = 0) +
+    blank_theme +
+    scale_fill_brewer(palette = "Blues") +
+    theme(axis.text.x=element_blank()) +
+    geom_text(aes_q(y = yy, label = yy_percent), size=5)
+  return(plot_temp)
+}
+
 # read data
 colClass <-
   c("integer",
-    "character",
-    "character",
-    "character",
     "factor",
     "factor",
-    "factor")
+    "factor",
+    "double",
+    "double",
+    "double")
 colNames <-
   c("learner_id",
     "country",
@@ -66,16 +123,7 @@ colnames(country_fisrt_10) <- colNamesCountry # add columns name
 colnames(country_last_1) <- colNamesCountry
 
 # create plot (bar) from first 10 country
-plot_country_10 <-
-  ggplot(data = country_fisrt_10, aes(x = Country, y = Frequency)) +
-  geom_bar(stat = "identity", width = 0.8, fill = "cadetblue3") +
-  geom_text(
-    aes(label = Frequency),
-    vjust = -0.2,
-    color = "black",
-    size = 3.5
-  ) +
-  theme_minimal()
+plot_country_10 <- plot_bar(country_fisrt_10,"Country","Frequency",angle=0)
 
 
 ## in course
@@ -85,26 +133,18 @@ names(course_vec)[names(course_vec) == "t"] <- "True" # change t to True
 course_vec <- data.frame(course_vec,colClass <- c("factor","integer"))
 colNamesCourse <- c("Teacher", "Frequency") # create columns names
 colnames(course_vec) <- colNamesCourse # add columns name
-
-# create blank theme (for pie chart)
-blank_theme <- theme_minimal()+
-  theme(
-    axis.title.x = element_blank(),
-    axis.title.y = element_blank(),
-    panel.border = element_blank(),
-    panel.grid=element_blank(),
-    axis.ticks = element_blank(),
-    plot.title=element_text(size=14, face="bold")
-  )
-# pie graph
-plot_course <-
-  ggplot(course_vec, aes(x = "", y = Frequency, fill = Teacher)) +
-  geom_bar(width = 1, stat = "identity") +
-  coord_polar("y", start = 0) +
-  blank_theme +
-  scale_fill_brewer(palette = "Blues") +
-  theme(axis.text.x=element_blank()) +
-  geom_text(aes(y = Frequency/2 + c(0, cumsum(Frequency)[-length(Frequency)]), 
-                label = scales::percent(Frequency/number_of_part_no_na)), size=5)
+## create plot (pie) course
+plot_course <- plot_pie(course_vec,"Teacher", "Frequency")
 freq_f = scales::percent(course_vec$Frequency[course_vec$Teacher == "False"]/number_of_part_no_na)
 freq_t = scales::percent(course_vec$Frequency[course_vec$Teacher == "True"]/number_of_part_no_na)
+
+## chapter
+chapter_vec <- data2018$unit # factor from unit column
+chapter_vec = factor(chapter_vec,levels(chapter_vec)[c(1,13,5,14,6,15,7,16,8:12,2:4,17)]) #reorder levels in factor (to ascending)
+chapter_vec <- data.frame(table(chapter_vec))
+colNamesCourse <- c("Unit", "Frequency") # create columns names
+colnames(chapter_vec) <- colNamesCourse # add columns name
+# create plot (bar) from first 10 country
+plot_unit <- plot_bar(chapter_vec,"Unit","Frequency",angle=45)
+
+
